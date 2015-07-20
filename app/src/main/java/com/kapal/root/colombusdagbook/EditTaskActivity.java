c006e485795528427722cdd4ee2c1062919f77c6
Kalpa Gunarathna <chathuranga456@gmail.com>
2015-07-20 22:28:47 +0530

    A working copy

--- app/src/main/java/com/kapal/root/colombusdagbook/EditTaskActivity.java
+++ app/src/main/java/com/kapal/root/colombusdagbook/EditTaskActivity.java
@@ -28,31 +28,48 @@ public class EditTaskActivity extends Activity {
 
         int x = (int) getIntent().getExtras().getLong("id");
         String s = ""+id;
-        Log.d(TAG,"ID is : "+s);
+        Log.d(TAG, "ID is : " + s);
 
 
 
         db = openOrCreateDatabase(dbh.DB_NAME, MODE_PRIVATE, null);
-        Log.d(TAG,"DB opened!");
+        Log.d(TAG, "DB opened!");
 
         Cursor c = db.rawQuery(dbh.getRow(x), null);
-        Log.d(TAG,"Cursor OK !");
+        Log.d(TAG, "Cursor OK !");
 
         try {
             c.moveToFirst();
             Log.d(TAG, "Moved to first");
+            Log.d(TAG, c.getColumnName(0));
+            Log.d(TAG, c.getString(c.getColumnIndex("_id")));
+            Log.d(TAG, c.getString(c.getColumnIndex("TASK_LOCATION")));
+            Log.d(TAG, c.getString(c.getColumnIndex("TASK_DESCRIPTION")));
+            Log.d(TAG, c.getString(c.getColumnIndex("TASK_LATITUDE")));
+            Log.d(TAG, c.getString(c.getColumnIndex("TASK_LONGTITUDE")));
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
-        /*id.setText(c.getString(c.getColumnIndex("_id")) );
-        location.setText(c.getString(c.getColumnIndex("TASK_LOCATION ")));
-        desc.setText(c.getString(c.getColumnIndex("TASK_DESCRIPTION ")));
-        lat.setText(c.getString(c.getColumnIndex("TASK_LATITUDE")));
-        longt.setText(c.getString(c.getColumnIndex("TASK_LONGTITUDE")));
-*/
 
+        /*String a1 = c.getString(c.getColumnIndex("_id")).toString() ;
+        String a2 = c.getString(c.getColumnIndex("TASK_LOCATION ")).toString();
+        String a3 = c.getString(c.getColumnIndex("TASK_DESCRIPTION ")).toString();
+        String a4 = c.getString(c.getColumnIndex("TASK_LATITUDE")).toString();
+        String a5= c.getString(c.getColumnIndex("TASK_LONGTITUDE")).toString();*/
+
+        String a1 = c.getString(0) ;
+        String a2 = c.getString(1);
+        String a3 = c.getString(2);
+        String a4 = c.getString(3);
+        String a5 = c.getString(4);
+
+        id.setText(a1);
+        location.setText(a2);
+        desc.setText(a3);
+        lat.setText(a4);
+        longt.setText(a5);
 
 
 
